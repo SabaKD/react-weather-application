01e15d8cabd1265b01110036bd93991a8fb1c022
SabaKD <saba.zadeh96@gmail.com>
2024-07-10 13:54:50 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    "Hi"
+export default function App() {
+  return(
+  <div className='App'>
+    <div className='container'>
+    <h1>
+      Weather Application
+    </h1>
+    <button className='btn btn-primary'>Hello</button>
+    </div>
+  </div>
   );
 }
 
-export default App;
