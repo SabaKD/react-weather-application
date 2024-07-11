002aeca05c975759d4ab7e9f0101a183b8f654f1
SabaKD <saba.zadeh96@gmail.com>
2024-07-11 14:58:42 +0200

    added the formatted date

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React , { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather(){
-    return (
+    const [weatherData, setWeatherData] = useState({ready : false});
+    function handleResponse (response){
+        // console.log(response.data.time);
+        setWeatherData({
+            ready: true,
+            city:"Lisbon",
+            temperature: response.data.temperature.current,
+            wind: response.data.wind.speed,
+            humidity: response.data.temperature.humidity,
+            description: response.data.condition.description,
+            feelsLike: response.data.temperature.feels_like,
+            icon: response.data.condition.icon_url,
+            date: new Date(response.data.time *1000)
+
+        })
+        
+    }
+
+    function search(){
+        let apiKey = "4bc76te01aac743d12o764377f46072e";
+        let cityName = "Moscow";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+    
+
+    if (weatherData.ready){
+        return (
         <div className="Weather">
             <form>
                 <div className="row">
@@ -19,10 +48,10 @@ export default function Weather(){
                 <div className="col-6 left-side">
                     <div className="row">
                         <div className="col-2">
-                            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="cloudy"/>
+                            <img src={weatherData.icon} alt={weatherData.description} className=" img-fluid weather-icon"/>
                         </div>
                         <div className="col-2">
-                            <span className="temprature">23 </span> 
+                            <span className="temperature">{Math.round(weatherData.temperature)} </span> 
                         </div>
                         <div className="col-3">
                             <span className="unit">ºC | ºF</span>
@@ -31,13 +60,13 @@ export default function Weather(){
                         <div className="col-5">
                             <ul>
                                 <li>
-                                    Precipitation: 11%
+                                    Feels like: {Math.round(weatherData.feelsLike)}
                                 </li>
                                 <li>
-                                    Humidity: 66%
+                                    Humidity: {weatherData.humidity}%
                                 </li>
                                 <li>
-                                    Wind: 18 km/h
+                                    Wind: {Math.round(weatherData.wind)} m/s
                                 </li>
 
                             </ul>
@@ -46,13 +75,13 @@ export default function Weather(){
 
                 </div>
                 <div className="col-6 right-side">
-                   <h1>Weather of CityName</h1>
+                   <h1>Weather of {weatherData.city}</h1>
                     <ul>
                         <li>
-                            Date
+                            <FormattedDate date={weatherData.date}/>
                         </li>
                         <li>
-                            description
+                            {weatherData.description}
                         </li>
 
                     </ul>
@@ -63,4 +92,26 @@ export default function Weather(){
         </div>
         
     );
+    } else {
+        search();
+        return ( 
+            
+            <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on"/>
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                    </div>
+                </div>
+            </form>
+            <div>
+                <strong>Loading...</strong>
+            </div>
+            </div>
+        );
+    }
+    
 }
